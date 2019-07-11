8a31e877ab27cd8196183a6e860b841c68b6536e
Siddharth Chugh <siddharthandroiddev@gmail.com>
2019-07-11 23:12:23 +0530

    new commit

---
+++ app/src/main/java/growithbig/technest/scheduledtask/Model/CurrentTaskDataitems.java
@@ -0,0 +1,51 @@
+package growithbig.technest.scheduledtask.Model;
+
+public class CurrentTaskDataitems {
+
+
+    private String id;
+    private String clientname;
+    private String client_address;
+    private String client_landmark;
+    private String client_contactno;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getClientname() {
+        return clientname;
+    }
+
+    public void setClientname(String clientname) {
+        this.clientname = clientname;
+    }
+
+    public String getClient_address() {
+        return client_address;
+    }
+
+    public void setClient_address(String client_address) {
+        this.client_address = client_address;
+    }
+
+    public String getClient_landmark() {
+        return client_landmark;
+    }
+
+    public void setClient_landmark(String client_landmark) {
+        this.client_landmark = client_landmark;
+    }
+
+    public String getClient_contactno() {
+        return client_contactno;
+    }
+
+    public void setClient_contactno(String client_contactno) {
+        this.client_contactno = client_contactno;
+    }
+}
